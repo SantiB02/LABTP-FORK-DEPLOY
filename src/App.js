cabcabbcd3d5d479ef59f19ff954adf0dcb58d3e
Leandro Cabral Santana <100740901+Leandrocabralsantana@users.noreply.github.com>
2023-09-18 14:50:19 -0300

    Feature: Primer push
    Se crea el proyecto, se configura tailwind con variables globales y se estructuran las carpetas a utilizar en el mismo.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
-
+import Footer from './pages/Footer';
 function App() {
   return (
     <div className="App">
+
+      <Footer />
+
       
     </div>
   );
---
+++ src/pages/Footer.jsx
@@ -0,0 +1,9 @@
+
+
+const Footer = () => {
+  return (
+    <div className="bg-cream-100">Este es el footer</div>
+  )
+}
+
+export default Footer
