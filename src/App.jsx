cfabcb4b01ae1e9863a1afc825bdec705c986bf4
SantiB02 <santibrasca02@gmail.com>
2023-09-30 17:58:42 -0300

    vuelta al renderizado anterior

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import "./App.css";
 import { NavBar } from "./pages/components/NavBar";
 import { Home } from "./pages/Home";
 import { Contact } from "./pages/Contact";
-import { UserProvider } from "./contexts/userContext";
 import { Register } from "./pages/Register";
+import { UserProvider } from "./contexts/userContext";
 
 export const userContext = React.createContext();
 
@@ -12,8 +12,9 @@ function App() {
   return (
     <>
       <UserProvider>
+        <Home />
         <NavBar />
-        <Register />
+        <Contact />
       </UserProvider>
     </>
   );
